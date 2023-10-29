096d729e436677ef03c0514e378a93020c9e7df1
Tosin Akinnifesi <dalesk1987@gmail.com>
2023-10-29 02:48:41 -0500

    Adding package.json and start script

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 clear;
-export NODE_ENV=PROD
-until node server/server.bundle.js; do
-    echo "Server crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+export NODE_ENV=PROD;
+node server/server.bundle.js; 
 done
